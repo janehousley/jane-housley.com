518e046e559e26f5df7b2b1418864edb7eb9d581
janehousley <janehousley@gmail.com>
2025-06-23 20:35:58 -0600

    adding were all taking care of each other blog post

---
+++ janehousley/src/app/blog/posts/Were-all-taking-care-of-eachother/page.tsx
@@ -0,0 +1,106 @@
+import BlogPostLayout from "@/app/components/BlogPostLayout";
+
+export default function Were_all_taking_care_of_eachother() {
+    return (
+        <BlogPostLayout title="We&apos;re all taking care of each other" subtitle="I&apos;ve waxed intellectual on this blog, and it&apos;s time to return to my roots (melodrama)." date="23 June 2025">
+            <p>
+                When I was 18 years old, I moved into the dorms of Brigham Young University in Provo, Utah. 
+                Having been raised in California and Arizona, I didn&apos;t know the difference between a <em>coat</em> and a <em>jacket</em>. I didn&apos;t even own a coat, except for my ski coat, which I called my ski jacket. 
+            </p>
+            <p>
+                One Sunday morning, as was ritual, my roommate Lexie (also an Arizona native) and I dressed for church and went 
+                to the lobby to wait for our friends to walk to church together. It was early November, and outside, wispy snow 
+                was falling on green grass that had been watered by a rain fallen only a few days before. The air temperature 
+                had, as instantaneously as a light switch, just crossed the threshold that crystallized rain into snow and 
+                catalyzed the unpacking of winter wear from storage closets, a custom Lexie and I were altogether unfamiliar 
+                with. Claire Hendricks (Colorado native) met us on the ground level of the dorm and asked &quot;where are your 
+                coats?&quot; My zip-up hoodies and sweatshirts had remained dutifully in my closet, unfit for the Sunday best 
+                occasion. A puffy, waterproof coat that sounded like plastic wrap when your arms brushed your sides? Not 
+                something Lexie or I owned. We had done our best with long-sleeve dresses. Completely unsatisfied by our sad 
+                attempt at warmth, Claire brought us to her room, where she lent a coat to each of us for the quarter-mile 
+                walk to the classroom that doubled as a chapel on Sundays. 
+            </p>
+            <p>
+                The phrase &quot;taking care of someone&quot; often takes the shape of a matronly role in my mind, like 
+                how a mother takes care of her children. I grew up surrounded by powerful, charitable women who cooked 
+                dinner for each other, organized service projects for the impoverished, watched each other&apos;s children. 
+                I learned what it means to take care of someone by watching my mom teach our neighbors how to read and do 
+                math, watching my dad drive our elderly friend to church. 
+            </p>
+            <p>
+                I could list several more awe-inspiring, heart-warming examples, but what I&apos;ve come to love recently is 
+                the <em>casualness</em> in which we care of each other. It grows out of the most basic level of sympathy 
+                and takes the shape of a protection over one another. It&apos;s a natural aversion to seeing any one of 
+                our fellow humans suffer. And it manifests itself every day in subtle ways, like lending a coat to a friend.
+            </p>
+            <p>
+                In my college town, I and most everyone I interact with have semi-recently left our nests and learned the 
+                hard truths about self-sustainability and independence. But we&apos;re also rotating turns being Mom for 
+                each other. We plan each other&apos;s birthday parties. We scratch each other&apos;s backs. We wear each 
+                other&apos;s clothes and do each other&apos;s makeup. We exchange knowledge, talents, advice. 
+                We take turns driving each other and we <em>never</em> Venmo for gas because it&apos;ll all even out in the end. 
+                If it doesn&apos;t? Nobody was keeping score anyway. It&apos;s casual and unspoken, the way we&apos;re caring 
+                after each other.
+            </p>
+            <p>
+                We sit on the sidelines of each other&apos;s intramural sports, because our mothers and fathers and siblings 
+                live in different states now. We read each other&apos;s research papers and buy each other frozen yogurt 
+                and sing each other songs. 
+            </p>
+            <p>
+                Of course our friends are inclined to take care of us, but I&apos;ve been warmed by the realization that 
+                this is a natural human tendency that requires no prerequisite emotional connection. Even in the 
+                loneliest stages of life, strangers hold the door open for each other, tuck in each other&apos;s shirt tags, 
+                offer each other directions. We&apos;re all just walking around on this big blue planet holding each other&apos;s hands.
+            </p>
+            <p>
+                I&apos;m a subscriber of the gifts and favors economy. In fact, I am a disciple of the gifts and favors 
+                economy. Unfamiliar? It&apos;s something like this: instead of making every interaction financially 
+                transactional, we do favors for each other because we&apos;re all here on Earth for the first time and 
+                everyone needs a ride to the airport once in a while. I let you borrow my shirt today knowing somewhere 
+                down the line you&apos;ll share your french fries. It&apos;s a beautiful philosophy to subscribe to because 
+                it both encourages selflessness and admits humility. I want to help you, and one day I will probably need 
+                some help in return. Alexis Long, my dear friend, once wrote:
+            </p>
+
+            <p className="mx-auto text-left w-full max-w-prose">
+                The girls have formed a gifts and favors economy <br/>
+                Trading sweaters for sourdough <br/>
+                Wearing each other's pants <br/>
+                Doing each other&apos;s hair <br/>
+            </p>
+            <p>
+                It&apos;s one of my favorite pieces of poetry ever written. And it&apos;s all true! We swap, share, 
+                lend, help, hold, drive, hug, and listen. I lay in Sophie&apos;s bed while she combs my hair and reads aloud. 
+                The next day, she lies on the lawn while I tell her &quot;it&apos;ll all work out&quot;. 
+            </p>
+            <p>
+                I am often jolted alert by the realization that I have been moved out of my childhood home for five years. 
+                I have fed myself, watered myself, granted sunshine to myself, <em>kept myself alive</em> for five years. 
+                I am pleasantly surprised—but surprised nonetheless—at this realization. I recall the times I was fed, 
+                watered, and granted sunshine by others, which leads me to the conclusion that I have been kept alive by 
+                others. And that&apos;s kind of the whole point, isn&apos;t it? Every ride to the airport, every coat 
+                lent on a November day, every sourdough loaf baked and shared serves the purpose of making it a little 
+                easier for the recipient to be alive. Let us not be too proud to admit how often we sustain one another.
+            </p>
+            <p>
+                Acknowledging and appreciating all the times I&apos;ve been taken care of makes it that much easier to 
+                surrender my time to take care of others. I&apos;m as independent and self-sustaining as they come, 
+                and I have had to learn to relinquish the tight reins I hold on keeping my schedule and optimizing my time. 
+                But when I think of all of us brand-new humans skipping around on this earth, driving each other to airports 
+                and painting each other&apos;s fingernails and sharing sunscreen, I realize how wasted my life would be if 
+                I stayed in my lane, eyes fixed forward toward finish line. Everyone needs to take a turn being Mom once 
+                in awhile, because everyone <em>needs</em> Mom once in awhile.
+            </p>
+            <p>
+                The point I&apos;m getting at is that in between the grandiose gestures of service and the unrighteous 
+                fits of selfishness, we all have a natural inclination to take care of each other. It&apos;s as subtle as 
+                brushing a piece of lint off a friend&apos;s shirt or lending a curling iron. We&apos;re all so new at 
+                being human beings on Earth; I think that is why it tugs at my heartstrings so dramatically to see all 
+                of us naive little creatures granting each other small tokens of love. We&apos;re figuring it out for 
+                the first time together. We&apos;re all taking care of each other.
+            </p>
+
+        </BlogPostLayout>
+    );
+}
