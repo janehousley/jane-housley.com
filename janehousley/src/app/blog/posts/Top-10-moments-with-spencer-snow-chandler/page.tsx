f1d56f8ed72df984e3ae62c917d1f21690931164
janehousley <janehousley@gmail.com>
2025-10-19 11:17:55 -0500

    Adding spencer blog post

---
+++ janehousley/src/app/blog/posts/Top-10-moments-with-spencer-snow-chandler/page.tsx
@@ -0,0 +1,177 @@
+import BlogPostLayout from "@/app/components/BlogPostLayout";
+import Image from "next/image";
+
+export default function Top_10_moments_with_spencer() {
+    return (
+        <BlogPostLayout title="Top 10: Moments with Spencer Snow Chandler" subtitle="We earned twin degrees in mathematics with minors in laughing (and computer science)." date="17 July 2025">
+            <p>
+                On Sunday, our friends are throwing a goodbye party for Spencer and I before we move to the east 
+                coast and midwest, respectively, to &quot;sell our souls to corporate America.&quot; We both graduated with 
+                Master&apos;s degrees in math this April, and anyone who knows either of us knows that it&apos;s only fitting 
+                that we would have a conjoined farewell party.
+            </p>
+            <p>
+                Since our first years at BYU in 2020, we&apos;ve taken over 80 credit hours of classes together. That&apos;s about
+                25 classes. We were coincidentally put into the same pre-major cohort and consequentially put in the same 
+                classes for the entirety of freshman year. After that, we just started taking all of our classes together. 
+                Despite our formal training in applied mathematics, we&apos;re both going to work for big name companies as 
+                Software Developers. What gives?
+            </p>
+            <p>
+                When I tell others about Spencer, it&apos;s rare I need to give any sort of background information. Everyone knows 
+                and loves him, and those who have not had the pleasure of being introduced know about his witty comments 
+                via my Instagram story. If I had a dollar for every time someone asked if we were dating, I could probably 
+                afford to decline my return offer and move to DC with him.
+            </p>
+            <p>
+                He&apos;s my best friend!
+            </p>
+            <Image
+                src="/blog_thumbnails/spencer_dance.png"
+                alt="Spencer and I dancing."
+                width={850}
+                height={500}
+            />
+            <p className="italic">
+                We love to dance! Specifically to &quot;Sweet but Psycho&quot; or &quot;Primadonna&quot;, but we&apos;ll 
+                take any Pop diva with a fast beat.
+            </p>
+            <p>
+                While preparing for the move and the farewell party, I&apos;ve reflected on some of our best moments. 
+                Spencer is constantly making me laugh, but he also cares deeply about his friends and their 
+                feelings. When we have an argument, he&apos;s the first to say &quot;let&apos;s talk about this&quot;, and then 
+                the first to apologize. So while most of these stories are retold because of their comedic value, 
+                there&apos;s a foundational layer of empathy the shines through in everything Spencer does.
+            </p>
+            <p>
+                Here&apos;s my Spencer Chandler Top 10, in no particular order:
+            </p>
+            <ol className="list-decimal list-outside pl-6 space-y-2 mb-4">
+                <li>
+                    <strong>Four Square Club.</strong> Spencer started a Four Square Club and elected me 
+                    vice president which basically just meant I had to show up on time so he wasn&apos;t the 
+                    only one there at the beginning. I also sometimes got to be on aux. His commitment to 
+                    the bit is inspiring.
+                </li>
+                <li>
+                    <strong>Fortnight</strong> We went through a phase where we would throw a party called 
+                    &quot;FortNight&quot; which basically meant we built a fort and made everyone bring iPads and played 
+                    Roblox. We checked that people had Roblox downloaded on their phone or iPad before they were 
+                    allowed to enter. Definitely took it a little too seriously.
+                </li>
+                <Image
+                    src="/blog_thumbnails/spencer_fortnight.png"
+                    alt="Fortnight flyer."
+                    width={850}
+                    height={500}
+                />
+                <p className="italic">FortNight flyer</p>
+                <li>
+                    <strong>Math 290 Group Quiz.</strong>The way Spencer and I went from classmates to friends 
+                    was no coincidence. He likes to say he &quot;masterminded&quot; it. In our first semester together, 
+                    our professor assigned a take-home quiz that we were allowed to work on with a group of 2-4 
+                    students. Spencer hand-picked me and another kid to do the quiz together (my greatest honor), 
+                    and the rest is history. After a session of working on the quiz, I met Maggie Dru, who became 
+                    one of my best friends. Talk about butterfly effect! It is an inspiring tale of budding 
+                    friendship—its warmth only dimmed by the fact that we got the lowest grade in the class. I would 
+                    claim irony, but it&apos;s honestly pretty fitting. Grateful for the lasting friendships; wish we tried 
+                    harder on the quiz.
+                </li>
+                <li>
+                    <strong>Running for Class President.</strong> At the beginning of our junior year, Spencer and I ran for ACME 
+                    Class President and Vice President against one (1) opposing team—and lost. Spencer accused 
+                    everyone of sexism. At the start of senior year, we debated running again, but couldn&apos;t decide 
+                    if it would be more embarrassing to lose 2 years in a row or to actually win.
+                </li>
+                <li>
+                    <strong>Woke Final Projects.</strong> Our comedic failures in school are not in short supply. 
+                    At the end of each semester of our senior year, we were tasked with completing a final project 
+                    with a group of students. Spencer, Jake, Brynn, and I always did our projects together. Being 
+                    the social justice warriors we are, we nobly opted to do a data science project in mathematically 
+                    proving the existence of a gender wage gap. The results? Inconclusive. Not a good look for our 
+                    loud-minority-left-leaning group of future diversity hires. The next semester, we decided to do 
+                    a project in predicting the length and symptoms of menstrual cycles. Another brazen feat in the 
+                    yes of our cohort of mostly men. (&quot;You&apos;re doing your project on…periods?&quot;) The results were again… 
+                    inconclusive. No problem!! We laughed it off while our right-leaning counterparts held their heads 
+                    a little higher, probably.
+                </li>
+                <li>
+                    <strong>Utah Lake.</strong> One day Spencer called me and told me to be ready in 5: he was 
+                    taking me to the lake. I don&apos;t remember what I had been complaining to him about that day: 
+                    boys, a headache, school, being tired. Whatever it was, he told me it was time to take me to 
+                    the lake to heal me &quot;like how they used to take women to the sea when they had devils in them.&quot; 
+                    We sat in his car at Utah Lake reading books and chatting until I felt better. This story cracks 
+                    me up every time I tell it, but it also is a testament to how deeply Spencer cares about his 
+                    friends in a very authentic way.
+                </li>
+                <Image
+                    src="/blog_thumbnails/spencer_tshirt.png"
+                    alt="Spencer and his t-shirt."
+                    width={850}
+                    height={500}
+                />
+                <p className="italic">
+                    During his time in Provo, Spencer took advantage of BYU&apos;s no-profit t-shirt printing press. 
+                    He pushed the boundaries of what could be printed at the university-sanctioned press, and got 
+                    away with more than I would have bet on!
+                </p>
+                <li>
+                    <strong>Drive home from Santa Clarita in 2025.</strong> We&apos;ve had our fair share of roadtrips 
+                    to visit our friends in the LA area. This year, we went on a spontaneous trip (20 minutes notice) 
+                    with Ayla and Mia. All weekend, we marveled at how such an impulsive trip could turn out so 
+                    flawless. We spoke too soon. On the drive home, a semi-truck oil spill closed down the highway, 
+                    and we were about 2 hours delayed. Traffic compounded and our total travel time was looking to be 
+                    upwards of 14 hours. We bickered over whether to stop in Vegas for a quick round of Roulette. 
+                    Having been friends for so long, we really do argue like siblings (&quot;Fine—no it&apos;s FINE.&quot;, &quot;Obviously 
+                    it&apos;s really important to you so let&apos;s just stop for Roulette.&quot;) Tensions were high on that drive. 
+                    When we stopped at a Wendy&apos;s for dinner, Spencer requested we take some alone time and sit at 
+                    different tables, which we did. Now we look back on that day and laugh.
+                </li>
+                <li>
+                    <strong>Dynamite.</strong> There was about a week during our junior year when I had to send Spencer the 
+                    homework every day because he would go home from class and play Just Dance for 4-6 hours 
+                    to try and get the world record for Dynamite. He had no time for homework. And you know what? 
+                    He beat the world record. Can&apos;t hate the hustle.
+                </li>
+                <li>
+                    <strong>Freshman Orientation.</strong> It was a warm August night in 2022 and Sophie, Spencer, 
+                    and I were looking for something to do. We were right on the cusp of school beginning, still in 
+                    the unbothered, blissful state that is homework-less-ness. Then, inspiration struck as Spencer 
+                    suggested that we go to freshman orientation…as juniors (Sophie a rising Senior). And we did! 
+                    Spencer ended up in some dating contest on stage (?) It honestly was all a blur, and we might 
+                    have scared some freshman, but I might have never laughed harder. There are cathedrals (Freshman orientations) 
+                    everywhere for those (non-Freshmen) with the eyes to see. 
+                </li>
+                <Image
+                    src="/blog_thumbnails/spencer_freshman_orientation.png"
+                    alt="Spencer on stage at freshman orientation."
+                    width={850}
+                    height={500}
+                />
+                <p className="italic">Spencer on stage at freshman orientation</p>
+                <li>
+                    <strong>Cosmos Kitties.</strong> Spencer captained and coached a very successful intramural 
+                    inner tube water polo team, Cosmos Kitties. I had the honor of being on the inaugural team, 
+                    from which I was swiftly cut before the next season. No prob, I was better as a cheerleader! 
+                    He has a unique way of bringing people together in the funniest ways: obscure intramural sports, 
+                    oddly-specific party themes, even school assignments. Put simply: he lives, he loves, he laughs. 
+                    I&apos;m grateful to have been in his circle during my time at BYU—it has made it all the better.
+                </li>
+                <Image
+                    src="/blog_thumbnails/spencer_grad.png"
+                    alt="Spencer and I graduating."
+                    width={850}
+                    height={500}
+                />
+                <p className="italic">We graduated together! Twice! Laughing of course.</p>
+                <p>
+                    I&apos;ll end it here before I wax too sappy and melodramatic. Spencer is one of my best friends, 
+                    and my college experience would be fundamentally and exponentially different without him.
+                </p>
+                <p>
+                    Love you!
+                </p>
+                
+            </ol>
+        </BlogPostLayout>
+    )}
