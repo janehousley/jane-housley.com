7f9c5cd7a076c04a76b43766ffc27c2b688e0e28
janehousley <janehousley@gmail.com>
2025-10-19 12:18:27 -0500

    adding the grief and the gap blog post

---
+++ janehousley/src/app/blog/posts/The-grief-and-the-gap/page.tsx
@@ -0,0 +1,150 @@
+import BlogPostLayout from "@/app/components/BlogPostLayout";
+import Image from "next/image";
+
+export default function The_grief_and_the_gap() {
+    return (
+        <BlogPostLayout title="The Grief and the Gap" subtitle="My recent thoughts about grief and loss and death and what to say." date="9 August 2025">
+            <p>
+                I recently read Lifeform by Jenny Slate. The chapter &quot;The Swan&quot; provided one of the most beautiful 
+                illustrations of grief and loss I&apos;ve probably ever read. Here&apos;s my favorite excerpt from that chapter:
+            </p>
+            <p>
+                &quot;It seemed cruel to me that we patiently live through periods of anguish and shock when we lose 
+                our family members, and we keep facing it, believing that activity and time will settle it all 
+                down for us. We make a deal with fate: I&apos;ll keep this flame that signifies the one you took. I 
+                will let it scorch me in my heart if you let it die down naturally, and eventually there will 
+                just be a scar on my heart, and I will always know what I have lost. But by then, I will feel 
+                only the emptiness, not the terrible scald. I will let the fire of the loss run its course. This 
+                is the debt I will pay so that I can have a more bearable sadness.
+            </p>
+            <p>
+                If you were looking at us from somewhere else, looking at the humans, you would notice that 
+                there is no way for us to have our loves without breathtaking pain, not because we love brutally 
+                but because we lose each other at different times. We don&apos;t get to end together as one.&quot;
+            </p>
+            <p>
+                I&apos;m nondiscriminatory about the things I write here, the only criteria being that I&apos;ve thought 
+                about it for long enough that I feel the urge to put it into words. To box it up, to package it 
+                nicely in a digestible, contained form. Recently I&apos;ve been thinking a lot more about more difficult 
+                topics, namely grief and loss and loneliness. So today I am writing from a place of softness and 
+                rawness and there&apos;s not much more to say about that. If I were speaking this, it would be delivered 
+                in the same gentle tone that moms say out-loud prayers in.
+            </p>
+            <p>
+                Through my grieving process, I found myself lingering on anger for a long time. I was angry at 
+                all the typical things like God and the continuous rotation of the Earth. I was angry my corporate 
+                PTO Bereavement ended, as if the world was saying: &quot;grieving due EOW.&quot; I was angry at those around 
+                me who occasionally forgot what happened and misspoke. I was angry at those who tiptoed around me 
+                like I was a fragile, small mouse squished under a huge elephant in the room. I was angry at those 
+                who didn&apos;t know the elephant existed. I was most angry at those who had seen and met and discussed 
+                and apologized for the elephant and then promptly forgot it existed, because I could not forget it 
+                existed, because I was being crushed under it every day. For a very long time, I had very little patience.
+            </p>
+            <p>
+                My anger eventually dissipated into some form of frustrated understanding. I knew it didn&apos;t make 
+                logical sense to expect my friends to hold my loss in the forefront of their minds at all times 
+                like I did. I knew there would be an occasional slip-up and I knew that the cause was not malice 
+                nor apathy. I knew they did not think of him as often as I did, of course. My life was not the center 
+                of their thoughts, just as theirs wasn&apos;t at the center of mine. Many of them were grappling with their 
+                own hardships. That&apos;s just how it is.
+            </p>
+            <p>
+                Outside of my family, the next person closest to me, my best friends, probably thought about him 
+                only a small fraction as often as I did. Although I understood how natural and innocent it was, 
+                the gap in how much I thought about him and how much the next closest person to me thought of him 
+                was really painful to come to terms with. I started thinking constantly of this Gap, even coining 
+                the name. &quot;The Gap felt wide today&quot; I would write in my journal. I visualized it like the Grand Canyon. 
+                I was standing on the South Rim and my best friends, who I knew loved and cared for me more than I 
+                deserved, still felt so far away. They were standing on the North Rim. My friendships felt like when 
+                you&apos;re young and you learn that the moon is actually very close to the earth compared to the sun. 
+                But then again, you realize, the moon is still very very far away.
+            </p>
+            <p>
+                As mortal beings with cognitive limits and flaws, we are simply incapable of completely closing the Gap. 
+                I simply do not have the capacity to think constantly of those that my friends have loved and lost. 
+                I can&apos;t surgically remove the weight of their grief and attach it to myself. But that&apos;s not really 
+                the point. Because even when my friends felt a Grand Canyon away, one baby step toward me, one &quot;hey, 
+                I&apos;ve been thinking about you,&quot; made a world of difference. Suddenly it didn&apos;t matter how wide the 
+                Gap was because someone was disregarding it and moving in my direction. The point is to take the 
+                steps, trusting that those steps make a difference, despite knowing we can never close the Gap 
+                completely. To know, logically, that you cannot feel what I feel, you cannot stand on the South Rim 
+                with me, and you cannot bring me to the North Rim right now, because there is cement around my ankles 
+                and I am scheduled to be here on the South Rim for a while, and you cannot come over here because 
+                I am grieving a person you never met. But you will try to walk toward me anyway because despite the 
+                distance being miles, despite knowing you cannot stand where I stand, we both believe it is better 
+                for us to be a little bit closer. And although most of the time the distance feels like a Grand 
+                Canyon apart, you take one step toward me and I realize we are actually only a few feet away. Taking 
+                that step is witnessing someone&apos;s pain, and it&apos;s actually a deeply Christian philosophy.
+            </p>
+            <p>
+                My constant fixation on the Gap eventually sparked a spiritual metamorphosis inside of me, in which 
+                I realized that even when it felt like everyone was on the North Rim, there was someone standing on 
+                the South Rim with me, and His name was Jesus Christ, and that changed my life.
+            </p>
+            <p>
+                I recently moved to a new state and people here often ask me why I&apos;ve decided to remain a member of 
+                the Church of Jesus Christ of Latter-Day Saints after all these years, after all these controversies, 
+                after all these Hulu shows. Last year I came to understand that one of the most important things we 
+                can do with our lives is to try to minimize the Gap. I then realized that when I was baptized at 8 
+                years old, I made a covenant to &quot;mourn with those that mourn, comfort those that stand in need of comfort.&quot; 
+                Fifteen years ago, I promised God Himself that I would do my very best to Mind the Gap.
+            </p>
+            <p>
+                I&apos;ve thought a lot about what Minding the Gap looks like in practice. Firstly, I think we are too 
+                avoidant with grief. When you&apos;ve lost someone, people fear accidentally &quot;bringing it up.&quot; But why? 
+                I&apos;m thinking about him all the time anyway so there&apos;s no way to remind me. He&apos;s already on my mind. 
+                And what&apos;s more, he&apos;s on my mind, but I don&apos;t know if I can say that out loud because I don&apos;t want 
+                to make you uncomfortable or make things awkward. But when you&apos;re thinking about something for hours 
+                and hours for a day, and you&apos;re not saying the thing out loud, you are drowning yourself. You are 
+                taking too many breaths in with not enough breaths out.
+            </p>
+            <p>
+                Navigating the social parameters of talking about death began to feel like carrying a bulky gray 
+                plastic thing around at all times. I stumbled under it&apos;s awkwardness and often floated to the 
+                perimeters of social conversation, standing at a distance where I had space for this bulky gray 
+                thing without bumping anyone&apos;s heads. I couldn&apos;t decide whether to speak in past or present tense, 
+                so I said nothing at all. But then saying nothing felt like a dishonoring, and I felt guilty, 
+                and I was trying to master this social perimeter of talking about (or avoiding talking about?) 
+                death but I was already too exhausted in my heart from holding the <em>weight</em> of loss that I didn&apos;t 
+                quite know how to manage the <em>bulk</em> of it too. And that is when the bulky gray thing was the most 
+                bulky and gray.
+            </p>
+            <p>
+                But sometimes I was relieved of the bulk! When someone asked directly about him, the bulky gray thing 
+                disappeared. I did not need to worry about making it awkward. I did not need to use the phrase &quot;sorry 
+                if I am trauma-dumping&quot;. I did not need to apologize for the heaviness of a situation that was not my fault at 
+                all, that I did not feel equipped to carry in the first place. I did not need to rationalize, preface, feign, 
+                back-pedal, or pretend. I could take a breath out. I could look across the huge mouth Grand Canyon and 
+                see it closing.
+            </p>
+            <p>
+                I do not subscribe to kitschy phrases like &quot;you don&apos;t owe anyone anything&quot; or &quot;put yourself first.&quot; 
+                I understand the sentiment behind them, I understand self care, I understand extreme people-pleasing. 
+                I will take care of myself and love myself and be kind to myself but I will never subscribe to the 
+                belief that I am in any way more important than or better than anyone else. I do not want to live 
+                in a world where no one owes each other anything. We owe each other visibility at the very least 
+                and more commonly we owe each other love. We owe each other listening ears and cry-on-me shoulders. 
+                We owe it to each other to make a priority of trying to close the Gap. I truly believe we are 
+                indebted to each other for these things.
+            </p>
+            <p>
+                The Gap is an inevitable. Not only in times of grief—the loneliness of the pain is a poltergeist 
+                that accompanies any of life&apos;s challenges. And the most discouraging thing about the Gap is that 
+                it cannot be closed. This is where the metaphor starts to break down because the physical world 
+                does not entirely encapsulate the emotional world. Because why would anyone start a hike they knew 
+                was impossible to finish? We cannot close the Gap. No amount of consolatory texts, therapeutic 
+                conversations, condolences flowers, home-cooked meals, apologetic hugs, tears shed, or errands 
+                run will make that distance 0. But, the steps still matter.
+            </p>
+            <p>
+                The steps matter because they eradicate the bulky gray thing and they lighten the horrible weight. 
+                The steps matter because God or evolution or both decided that emotional intelligence was just as 
+                necessary to our survival as technological intelligence, otherwise we wouldn&apos;t have it. 
+                The steps matter because there will be days where your heart is too exhausted to look at your 
+                scar left by the scalding flame and so someone else will witness it for you. And when that happens, 
+                it will be easy and obvious to see why the steps are so important, despite it all. Despite the 
+                impossibility of the goal they&apos;re trying to achieve. Empathy is the greatest underdog story we have.
+            </p>
+        </BlogPostLayout>
+
+    )}
